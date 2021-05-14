5e83415d80013587bd242f35a328d79835eb0a86
atemoia <renannowak@hotmail.com>
2021-05-14 18:19:20 -0300

    Feito o esqueletod o html e inicio do script

---
+++ scripts.js
@@ -0,0 +1,40 @@
+let firstNumber = null;
+let operator = null;
+let secondNumber = null;
+let result = null;
+let isDecimal = false;
+
+function operate (a, op, b) {
+    if (op === '*') {
+        return a * b;
+    } else if (op === '/') {
+        if (y === 0) {
+            return NaN;
+        } else {
+            return a / b;
+        }
+    } else if (op === '+') {
+        return a + b;
+    } else if (op === '-') {
+        return a - b;
+    }
+}
+
+function percent (a) {
+    return a / 100;
+}
+
+function ac (...args) {
+    let firstNumber = null;
+    let operator = null;
+    let secondNumber = null;
+    let result = null;
+    let isDecimal = false;
+}
+
+function del (...args) {
+    ;
+}
+function equal (...args) {
+    ;
+}
